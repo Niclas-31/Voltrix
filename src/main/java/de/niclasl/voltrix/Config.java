546989219ab4fcee29da39e928a04d43c8fd3f1e
Niclas-31 <niclasloeffler3105@gmail.com>
2026-08-06 13:20:27 +0200

    Initial commit

--- src/main/java/de/niclasl/voltrix/Config.java
+++ src/main/java/de/niclasl/voltrix/Config.java
@@ -7,5 +7,8 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
+    public static final ModConfigSpec.BooleanValue EXPERIMENTAL = BUILDER
+            .define("experimental", false);
+
     static final ModConfigSpec SPEC = BUILDER.build();
 }
